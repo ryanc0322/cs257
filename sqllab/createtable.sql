cc9905fb4f11831dee4d3fb58c8005ede18730df
ryanc0322 <choir@carleton.edu>
2024-01-17 17:11:02 -0500

    quey.sql file addition with queries of my own with the descriptions of what they do

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
   quakedate date,
-  quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
---
+++ sqllab/query.sql
@@ -0,0 +1,8 @@
+-- Find all of earthquakes that had a magnitude between 1 and 3, and quakedepth between 10 and 30.
+Query 1: SELECT * FROM earthquakes WHERE mag BETWEEN 1 AND 3 AND quakedepth BETWEEN 10 AND 30;
+
+-- Find all earthquakes where magnitude was greater than 5.
+Query 2: SELECT * FROM earthquakes WHERE mag > 5;
+
+-- Find all earthquakes where longitude is between 100 and 200 and quakedepth is between 10 and 20.
+Query 3: SELECT * FROM earthquakes WHERE longitude BETWEEN 100 AND 200 AND quakedepth BETWEEN 10 AND 20;
