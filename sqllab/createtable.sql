50ef337912f9d1143694c93f600be46c31d7ce1f
ryanc0322 <choir@carleton.edu>
2024-01-19 10:30:05 -0500

    version with timestamp successful

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
+  quaketime timestamp with time zone,
   latitude real,
   longitude real,
   quakedepth real,
--- sqllab/query.sql
+++ sqllab/query.sql
@@ -6,3 +6,6 @@ Query 2: SELECT * FROM earthquakes WHERE mag > 5;
 
 -- Find all earthquakes where longitude is between 100 and 200 and quakedepth is between 10 and 20.
 Query 3: SELECT * FROM earthquakes WHERE longitude BETWEEN 100 AND 200 AND quakedepth BETWEEN 10 AND 20;
+
+-- Find all earthquakes that happened between 12pm and 8pm with a magnitude greater than 3. 
+SELECT * FROM earthquakes WHERE extract(hour from quaketime) BETWEEN 12 AND 20 AND mag>3;
