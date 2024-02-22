a3271b4240278b8a400fef402d00f2cbc7ed4032
ryanc0322 <156211477+ryanc0322@users.noreply.github.com>
2024-02-22 12:43:20 -0600

    Update change_color.py

--- csslab/change_color.py
+++ csslab/change_color.py
@@ -8,13 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('/index.html')
 
-# update the background color of the webpage 
-@app.route('/random_color')
-def random_color():
-    colors = ['red','orange','yellow','green','blue','indigo', 'purple']
-    random_color = random.choice(colors)
-    return random_color
-
 if __name__ == '__main__':
     app.run(debug=True)
 
